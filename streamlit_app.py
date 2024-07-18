f759b726fc5e4d152fa67c7b58180ef569950758
eichemeg <megan.eichelberger@odot.oregon.gov>
2024-07-18 08:55:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
@@ -43,6 +45,5 @@ if ingredients_list:
       st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-import requests
 
 #st.text(fruityvice_response.json())
